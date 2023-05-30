1a44993f1f89a8dcd6d1c22d4550703021166740
Dejvi Ndreka <dejvi.ndreka@stud.hs-flensburg.de>
2023-05-30 18:51:47 +0200

    bring it all together

---
+++ VueTutorial/AppBackup.js
@@ -0,0 +1,8 @@
+import AppButton from ".js/components/AppButton.js";
+
+export default {
+
+        components: {
+          "app-button": AppButton,
+        },
+}
---
+++ js/components/Assignments.js
@@ -0,0 +1,38 @@
+import AssignmentList from "./AssignmentList.js";
+
+export default {
+  components: { AssignmentList },
+  // thats how he call up the AssignmentList.js, + the us of it at the template below
+
+  template: `
+
+  <section class="space-y-6">
+    <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+
+    <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+  </section>
+    `,
+
+  data() {
+    return {
+      assignments: [
+        { name: "Finish project", complete: false, id: 1 },
+        { name: "Read chapter 4 ", complete: false, id: 2 },
+        { name: "Turn in homework", complete: false, id: 3 },
+      ],
+    };
+  },
+
+  computed: {
+    filters() {
+      return {
+        inProgress: this.assignments.filter(
+          (assignment) => assignment.complete
+        ),
+        completed: this.assignments.filter(
+          (assignment) => !assignment.complete
+        ),
+      };
+    },
+  },
+};
