deb6f880fe113c5b10b1549fbabc6e48a1d7ae70
Dejvi Ndreka <dejvi.ndreka@stud.hs-flensburg.de>
2023-06-02 14:34:31 +0200

    inital commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- it's common use, to take The in front of Welcome if the Welcome page has only this usage of being needed once within the project -->
+    <TheWelcome />
+  </main>
+</template>
