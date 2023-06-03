783c6ba4a370f3627e3d7bf48f56b1e38381b679
Dejvi Ndreka <dejvi.ndreka@stud.hs-flensburg.de>
2023-06-03 21:34:24 +0200

    dependency injection with provide and inject

---
+++ src/components/Quiz/MyQuiz.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { provide, ref } from 'vue'
+
+defineProps({ quiz: Object })
+
+let name = ref('John Doe')
+
+//Quiz.vue will provide this value to all of it's children!
+//so if there is an useful variable there is no need of components that only handle the variable
+provide('key', {
+  name,
+  changeName: () => (name.value = 'Changed')
+})
+//the logic of changing the name can only be done by the parent
+</script>
+<!--  
+  So the child can call it like this:
+  <button @click="changeName">{{ name }}</buton>
+  or with "inject"
+
+  let quiz = inject('quiz');
+-->
+
+<template>
+  <h1>{{ name }}</h1>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,5 @@
-<script setup>
-import TabbableTextarea from '@/components/TabbableTextarea.vue'
-import { ref } from 'vue'
-let comment = ref('initial value')
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <form>
-      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px" />
-    </form>
-  </main>
+  <main></main>
 </template>
